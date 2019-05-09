74e0effa69255cca6e06379033e034cd28a9cdb2
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-05-08 19:20:08 -0500

    having metadata when starting with a species lists

--- circling_sh/get_checkLists.sh
+++ circling_sh/get_checkLists.sh
@@ -17,17 +17,25 @@ Positional argument:
 Optional arguments:
 -------------------
     ${BROWN}-a${NC} <string>, ${BROWN}--area-id${NC} <string>
-        Area ID. This variable will be directly used for OBIS databases
+        Area ID. This variable will be directly used for OBIS databases.
     ${BROWN}-n${NC} <string>, ${BROWN}--area-name${NC} <string>
-        Area name. This variable will be directly used for BOLD databases
+        Area name. This variable will be directly used for BOLD databases.
+    ${BROWN}-l${NC} <file>,   ${BROWN}--species-list${NC} <file>
+        File with a species list to search for.
+    ${BROWN}-r${NC} <string>, ${BROWN}--at${NC} <string>
+        Coupled with '--species-list' option and add, when used, a specific
+        taxonomical rank at metadata of its output.
+                    ${YELLOW}[default= Phylum]${NC}
     ${BROWN}-p${NC} <string>, ${BROWN}--output-prefix${NC} <string>
         Output prefix which will be used for naming validated names from both
-        OBIS and BOLD databases 
+        OBIS and BOLD databases. Please consider that default structure of names
+        are used to downstream analyses.
                     ${YELLOW}[default= <TAXON>_<obis|bold>_validated.txt]${NC}
-    ${BROWN}-h${NC}, ${BROWN}--help${NC}
+    ${BROWN}-h${NC},          ${BROWN}--help${NC}
         Show this help message and exit\n" 1>&2; exit 1; }
 
 if [ ! -e "${p}" ];then PREFIX="TAXA" ; fi
+if [ ! -e "${r}" ];then AT="any"; fi
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -55,6 +63,11 @@ case $key in
     shift
     shift
     ;;
+    -r|--at)
+    AT="$2"
+    shift
+    shift
+    ;;
     -h|--help)
     helping
     shift # past argument
@@ -68,7 +81,7 @@ done
 
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-#POSITIONAL="Anchoa"
+#POSITIONAL="Reptilia"
 #AREA_ID="38"
 #AREA_NAME="Colombia"
 
@@ -81,7 +94,8 @@ if [[ ! -z $SL ]]; then
 
     PREFIX=$AREA_NAME'_'$AREA_ID'_'${POSITIONAL[@]}
     cat $SL > ${POSITIONAL[@]}'_obis'
-    
+
+    if [[ `echo $AT` =~ "any" ]]; then AT="Phylum"; fi
 else
 
     # uuid=$(
@@ -147,28 +161,48 @@ echo -e "\nValidating names from OBIS and storing them at: ${BROWN}"$obi_file"\n
 IFS=$'\n'
 for i in $(cat ${POSITIONAL[@]}'_obis'); do
 
+    # i="Caretta caretta"
+
     echo $i
 
-    if [[ -z $(cat "backUp_obis" | grep -e "${POSITIONAL[@]},$i,") ]]; then
+    if [[ -z $(cat "backUp_obis" | grep -e "^${POSITIONAL[@]},$i,") ]]; then
 
         touch obi_syns
         while [[ $(cat obi_syns | wc -l) -lt 1 ]]; do
 
             python3 $OBC_PREFIX'/circling_py/select_ids.py'\
                     --tax \
-                    --type synonyms\
-                    --string "$i"\
-                    --prefix "${POSITIONAL[@]}" >> obi_syns
+                    --type   synonyms\
+                    --string "${i[@]}"\
+                    --prefix "${POSITIONAL[@]}"\
+                    --at     "${AT[@]}" >> obi_syns
 
         done
 
-        grep -e "${POSITIONAL[@]},$i," obi_syns | awk -F',' 'NR==1 {print $3}' >> $obi_file
-        cat obi_syns >> backUp_obis
+        if [[ ! -z $SL ]]; then
 
+            Valid=$( grep -e "^${POSITIONAL[@]},$i," obi_syns | awk -F',' 'NR==1 {print $3}' )
+            Rank=$(  grep -e "^${POSITIONAL[@]},$i," obi_syns | awk -F',' 'NR==1 {print $4}' )
+ 
+            echo "${Valid[@]},${AREA_NAME[@]},${Rank[@]},${POSITIONAL[@]}" >> $obi_file
+        else
+            grep -e "${POSITIONAL[@]},$i," obi_syns | awk -F',' 'NR==1 {print $3}' >> $obi_file
+        fi
+
+        cat obi_syns >> backUp_obis
         rm obi_syns
     else
 
-        cat "backUp_obis" | grep -Ee "^${POSITIONAL[@]},$i," | awk -F',' '{print $3}' >> $obi_file
+        if [[ ! -z $SL ]]; then
+
+            Valid=$( cat "backUp_obis" | grep -Ee "^${POSITIONAL[@]},$i," | awk -F',' 'NR==1 {print $3}' )
+            Rank=$(  cat "backUp_obis" | grep -Ee "^${POSITIONAL[@]},$i," | awk -F',' 'NR==1 {print $4}' )
+
+            echo "${Valid[@]},${AREA_NAME[@]},${Rank[@]},${POSITIONAL[@]}" >> $obi_file
+
+        else
+            cat "backUp_obis" | grep -Ee "^${POSITIONAL[@]},$i," | awk -F',' 'NR==1 {print $3}' >> $obi_file
+        fi
     fi
 
 done
@@ -190,16 +224,28 @@ for spps_valid in $(cat $obi_file); do
     ## take one validated species 
     ## then, obtain all synonyms
 
+    ## species list case:
+    if [[ ! -z $SL ]]; then 
+        ## metadata
+        md=','$(echo $spps_valid | cut -d ',' -f2-)
+        spps_valid=$( echo $spps_valid | cut -d ',' -f1 )
+        syn_patt="^${POSITIONAL[@]},.*,${spps_valid[@]},"
+    else
+        ## metadata
+        md=""
+        syn_patt="^${POSITIONAL[@]},.*,${spps_valid[@]}$"
+    fi
+
     ####### delete!! #########
-    #spps_valid="Anchoa hepsetus"
+    #spps_valid="Abyla haeckeli"
     ####### delete!! #########
 
     echo -e "${BROWN}"$spps_valid"${NC}"
 
-    for spps_syns in $(grep -Ee "^${POSITIONAL[@]},.*,$spps_valid$" backUp_obis | awk -F',' '{print $2}'); do
+    for spps_syns in $(grep -Ee "$syn_patt" backUp_obis | awk -F',' '{print $2}'); do
 
         ####### delete!! #########
-        #spps_syns="Anchoa hepsetus"
+        # spps_syns="Anchoa nasus"
         ####### delete!! #########
 
         echo -e "synonym: $spps_syns"
@@ -218,57 +264,35 @@ for spps_valid in $(cat $obi_file); do
 
             echo $main_string_bold >> backUp_bold
 
-            #echo ${POSITIONAL[@]}
-            #echo $spps_valid
-
             main_string_bold=$(echo $main_string_bold | sed -Ee "s/^${POSITIONAL[@]},/$spps_valid,/g")
-
-            if [[ -z $(echo $main_string_bold | grep "unavailable") ]]; then
-
-                if [[ -z $(echo $main_string_bold | grep "private") ]]; then
-
-                    if [[ -z $(echo $main_string_bold | grep -e "$AREA_NAME") ]]; then
-
-                        echo $main_string_bold | sed -Ee "s/(^$spps_valid,$spps_syns,).*/\\1public_outside/g" >> $bold_file
-                    else
-
-                        echo $main_string_bold | sed -Ee "s/(^$spps_valid,$spps_syns,).*/\\1public_inside/g" >> $bold_file
-                    fi
-                else
-
-                    echo $main_string_bold >> $bold_file
-                fi
-            fi
 
         else
 
             main_string_bold=$(echo $check_inBackUp | sed -Ee "s/^${POSITIONAL[@]},/$spps_valid,/g")
-
-            if [[ -z $(echo $main_string_bold | grep "unavailable") ]]; then
-
-                if [[ -z $(echo $main_string_bold | grep "private") ]]; then
-
-                    if [[ -z $(echo $main_string_bold | grep -e "$AREA_NAME") ]]; then
-
-                        echo $main_string_bold | sed -Ee "s/(^$spps_valid,$spps_syns,).*/\\1public_outside/g" >> $bold_file
-                    else
-
-                        echo $main_string_bold | sed -Ee "s/(^$spps_valid,$spps_syns,).*/\\1public_inside/g" >> $bold_file
-                    fi
-                else
-
-                    echo $main_string_bold >> $bold_file
-                fi
-            fi
         fi
 
+        if [[ -z $(echo $main_string_bold | grep "unavailable") ]]; then
+
+            if [[ -z $(echo $main_string_bold | grep "private") ]]; then
+
+                if [[ -z $(echo $main_string_bold | grep -e "$AREA_NAME") ]]; then
+
+                    echo $main_string_bold | sed -Ee "s/(^$spps_valid,$spps_syns,).*/\\1public_outside$md/g" >> $bold_file
+                else
+
+                    echo $main_string_bold | sed -Ee "s/(^$spps_valid,$spps_syns,).*/\\1public_inside$md/g" >> $bold_file
+                fi
+            else
+
+                echo $main_string_bold$md >> $bold_file
+            fi
+        fi
     done
 done
 
-
 if [[ $(cat $bold_file | wc -l) -eq 0 ]]; then
 
-    echo -e "\n${RED}There are not species available in BOLD by given parameters\n${NC}"
+    echo -e "\n${RED}There are not species available in BOLD by either given parameters or species\n${NC}"
 fi
 
 if [[ ! -z $SL ]]; then
